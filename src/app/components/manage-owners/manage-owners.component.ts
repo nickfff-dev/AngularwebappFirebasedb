8b882e599b206b1b826abb708a26502268762b50
nickfff-dev <hunchographics@gmail.com>
2021-11-28 15:58:25 +0300

    manageownersdb

--- src/app/components/manage-owners/manage-owners.component.ts
+++ src/app/components/manage-owners/manage-owners.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-manage-owners',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageOwnersComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private  store: AngularFirestore){
+    const users = this.store.collection('owners').valueChanges();
+  
+  }
+  model = { fname: "", lname: "", email: ""};
   ngOnInit(): void {
   }
+  addOwner(model: unknown) {
+    this.store.collection('owners').add(model);
+    };
+
+  removeOwner(model: string | undefined) {
+    this.store.collection('owners').doc(model).delete();
+    };
+  editOwner(model: string | undefined) {
+  this.store.collection('owners').doc(model).update(this.model);
+}
+
+ownerSubmit() {
+  this.addOwner(this.model);
+  this.model.fname = "";
+  this.model.lname = "";
+  this.model.email = "";
+}
+ownerDelete() {
+  this.removeOwner(this.model.fname);
+  this.removeOwner(this.model.lname);
+  this.removeOwner(this.model.email);
+  this.model.fname = "";
+  this.model.lname = "";
+  this.model.email = "";
+}
+ownerEdit() {
+  this.editOwner(this.model.fname);
+  this.editOwner(this.model.lname);
+  this.editOwner(this.model.email);
+  this.model.fname = "";
+  this.model.lname = "";
+  this.model.email = "";
+}
 
 }
